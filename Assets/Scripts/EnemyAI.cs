f7456ba23fe387f9aee33e0a5e8d849b7af918f3
CQuez <crq4376@rit.edu>
2021-04-19 11:02:41 -0400

    Added ground based enemy type
    Walks toward end of platform and back, deletes upon player contact

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private float DistanceFromLedge = .2f;
+    private float DistanceFromWall = .2f;
+
+    public GameObject player;
+    public GameObject Enemy;
+
+    private Rigidbody2D Rb;
+    private bool FaceRight = true;
+    private int goRight = 1;
+    private float speed = 1;
+
+    private bool OnGround;
+    private bool HitWall;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask whatIsGround;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Moves the enemy and flips direction when at a ledge or hitting a wall
+    /// </summary>
+    void Update()
+    {
+        OnGround = Physics2D.Raycast(groundCheck.position, Vector2.down, DistanceFromLedge, whatIsGround);
+        HitWall = Physics2D.Raycast(wallCheck.position, transform.right, DistanceFromWall, whatIsGround);
+
+        if (!OnGround || HitWall)
+        {
+            Flip();
+        }
+        else
+        {
+            movement.Set(speed * goRight, Rb.velocity.y);
+            Rb.velocity = movement;
+        }
+    }
+
+    /// <summary>
+    /// Flip the enemy's direction
+    /// </summary>
+    private void Flip()
+    {
+        FaceRight = !FaceRight;
+
+        goRight *= -1;
+
+        // Multiply local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    /// <summary>
+    /// Checks for a collision between the Rigidbodies of the player and this enemy.
+    /// </summary>
+    /// <param name="col"></param>
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.attachedRigidbody == player.GetComponent<Rigidbody2D>())
+        {
+            //Delete this Enemy
+            Destroy(this.gameObject);
+        }
+    }
+}
