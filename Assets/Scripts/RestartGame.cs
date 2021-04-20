abc4c8146392f4824b586a5e8b99afe290ac7136
Marc Molnar <mgm1349@rit.edu>
2021-04-20 14:36:41 -0400

    Timer and Platform Functionality.
    Enemies now add time to the timer. Timer does not count down in air and on certain platfomrs.

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Will Restart the game when the restart game button is clicked
+    /// </summary>
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
