014e895379cb9e42a3ee25e5181c329632e7a431
Marc Molnar <mgm1349@rit.edu>
2021-04-22 16:50:27 -0400

    Added Scenes
    Scene Selector, Level 2 and 3. Main Scene. Added functionality to go back and forth between scenes.

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class RestartGame : MonoBehaviour
 {
+    private const string previousLevelText = "Assets/Text Files/PreviousLevel.txt";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,11 @@ public class RestartGame : MonoBehaviour
     }
 
     /// <summary>
-    /// Will Restart the game when the restart game button is clicked
+    /// Will Restart the level when the restart game button is clicked
     /// </summary>
     public void LoadLevel()
     {
-        SceneManager.LoadScene(0);
+        int level = int.Parse(File.ReadAllText(previousLevelText));
+        SceneManager.LoadScene(level);
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int sceneNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Loads the scene number based on the integer value
+    /// that is given to this script.
+    /// </summary>
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
