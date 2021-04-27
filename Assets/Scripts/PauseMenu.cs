837bebb95a1c45c5c39958573b340cd5864bdd7a
Jacob Brosler <acingmc@gmail.com>
2021-04-27 00:51:58 -0400

    Made pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    bool paused = false;
+
+    /// <summary>
+    /// Pauses or unpauses the level when escape is pressed
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+                Unpause();
+            else
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+                paused = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Restarts the current level
+    /// </summary>
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Unpause();
+    }
+
+    /// <summary>
+    /// Unpauses the level
+    /// </summary>
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+}
